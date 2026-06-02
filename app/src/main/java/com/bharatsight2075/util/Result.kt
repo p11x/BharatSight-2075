985626c89949a34215b2b55f4fa6818c692cc5a7
p11x <p11x@github.com>
2026-06-02 08:40:34 +0530

    Complete App Upload: BharatSight 2075 Engineering Edition

--- app/src/main/java/com/bharatsight2075/util/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.bharatsight2075.util
-
-sealed class Resource<out T> {
-    data class Success<out T>(val data: T) : Resource<T>()
-    data class Error(val message: String, val exception: Throwable? = null) : Resource<Nothing>()
-    object Loading : Resource<Nothing>()
-}
